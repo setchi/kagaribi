521e29bb5a92b84bb1a47027ab5d32aeb5bfb739
setchi <ri_xer@yahoo.co.jp>
2015-01-26 00:04:21 +0900

    火の玉をランダムに点滅させる

---
+++ Assets/Scripts/Main/AutoBlink.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoBlink : MonoBehaviour {
+	public float fastestInterval;
+	public float latestInterval;
+	public float maxAlpha;
+	public float minAlpha;
+	public SpriteRenderer spriteRenderer;
+	Color color;
+
+	void Start () {
+		color = spriteRenderer.color;
+		StartCoroutine(Blink());
+	}
+
+	IEnumerator Blink() {
+		while (true) {
+			color.a = random(minAlpha, maxAlpha);
+			spriteRenderer.color = color;
+			yield return new WaitForSeconds(random(fastestInterval, latestInterval));
+		}
+	}
+
+	float random(float min, float max) {
+		return min + Random.value * (max - min);
+	}
+}
