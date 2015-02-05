9a8d3f375a172d43bf418c7bb9ce537eb3efaa8c
setchi <ri_xer@yahoo.co.jp>
2015-02-05 18:03:25 +0900

    ゲームオーバー時にBGMをフェードアウト

--- Assets/Scripts/Main/Managers/GameOverManager.cs
+++ Assets/Scripts/Main/Managers/GameOverManager.cs
@@ -4,13 +4,24 @@ using System.Collections;
 public class GameOverManager : MonoBehaviour {
 	public PlayerLife playerLife;
 	public FadeManager fadeManager;
+	public AudioSource audioSource;
 
 	bool gameOver = false;
 
 	void Update () {
 		if (!gameOver && playerLife.isDead) {
-			gameOver = true;
-			fadeManager.FadeOut(1f, EaseType.linear, () => Application.LoadLevel("Result"));
+			GameOver();
 		}
+	}
+
+	void GameOver() {
+		gameOver = true;
+
+		var volume = audioSource.volume;
+		TweenPlayer.Play(gameObject, new Tween(1f).ValueTo(Vector3.one, Vector3.zero, EaseType.linear, pos => {
+			audioSource.volume = volume * pos.x;
+		}));
+
+		fadeManager.FadeOut(1f, EaseType.linear, () => Application.LoadLevel("Result"));
 	}
 }
